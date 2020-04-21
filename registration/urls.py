57bfdb3ff9e7923782812a27ab10b7e0f1e96c5b
Daniel <danieldejesusgs@gmail.com>
2020-04-20 21:05:33 -0400

    :sparkles: Vistas login y registro

---
+++ registration/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import LoginPageView, RegisterPageView
+
+urlpatterns = [
+    
+    path('', LoginPageView.as_view(), name='login'),
+    path('registro/', RegisterPageView.as_view(), name='registro')
+  
+]
---
+++ registration/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
+
+
+class LoginPageView(TemplateView):
+    template_name = "registration/login.html"
+
+class RegisterPageView(TemplateView):
+    template_name = "registration/registro.html"

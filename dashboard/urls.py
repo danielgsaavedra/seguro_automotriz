cb61e67948b7d999f4f6b629ceb4eb6155ea4f0c
danieldeyisus <danieldejesusgs@gmail.com>
2020-04-28 01:10:36 -0400

    :sparkles: conexion con base de datos, se agrega listar usuarios

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,12 @@
   
 from django.urls import path
-from .views import DashboardView, SiniestrosView
+from .views import DashboardView, SiniestrosView, UsuariosView
+from . import views
 
 urlpatterns = [
     
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
-     path('siniestros/', SiniestrosView.as_view(), name='siniestros'),
+    path('siniestros/', SiniestrosView.as_view(), name='siniestros'),
+    path('usuarios/', views.UsuariosView, name='usuarios'),
   
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from .models import Usuario
 # Create your views here.
 
 class DashboardView(TemplateView):
@@ -7,3 +8,8 @@ class DashboardView(TemplateView):
 
 class SiniestrosView(TemplateView):
     template_name = 'dashboard/siniestros.html'
+    
+def UsuariosView(request):
+    usuarios = Usuario.objects.all().order_by('rol_id_rol')
+    context = {'usuarios': usuarios}
+    return render(request, 'dashboard/usuarios.html', context)

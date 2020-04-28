422d5e9fac84862663e7869db5dedc54f38339ba
Jota-Wolf <lobos.joaquin@hotmail.com>
2020-04-28 02:28:09 -0400

    :sparkles: Se crea listar polizas con su view y urls

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from .models import Usuario, Taller,Asegurado,Vehiculo
+from .models import Usuario, Taller,Asegurado,Vehiculo,Poliza
 
 # Create your views here.
 
@@ -29,3 +29,8 @@ def VehiculosView(request):
     vehiculos = Vehiculo.objects.all().order_by('anio')
     context = {'vehiculos': vehiculos}
     return render(request, 'dashboard/vehiculo.html', context)
+
+def PolizasView(request):
+    polizas = Poliza.objects.all().order_by('id_poliza')
+    context = {'polizas': polizas}
+    return render(request, 'dashboard/poliza.html', context)

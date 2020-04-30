418608f2b9c13dc25cfe070e5384fd15b1eb8630
danieldeyisus <danieldejesusgs@gmail.com>
2020-04-30 03:50:27 -0400

    :ambulance: se arreglo error update poliza y se cambio url por path

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('asegurados/create/', views.AseguradoCreate, name='asegurado_create'),
     path('asegurados/<str:id>/update/',views.AseguradoUpdate,name='asegurado_update'),
     path('vehiculos/', views.VehiculosView, name='vehiculos'),
-    url(r'^polizas/$', views.PolizasView, name='polizas'),
-    url(r'^polizas/create$', views.CreatePoliza, name='poliza_create'),
-    url(r'^polizas/(?P<id>\d+)/update$',views.UpdatePoliza,name='poliza_update'),
-    url(r'^polizas/(?P<id>\d+)/delete$',views.DeletePoliza,name='poliza_delete'),
+    path('polizas/', views.PolizasView, name='polizas'),
+    path('polizas/create', views.CreatePoliza, name='poliza_create'),
+    path('polizas/<str:id>/update',views.UpdatePoliza,name='poliza_update'),
+    path('polizas/<str:id>/delete',views.DeletePoliza,name='poliza_delete'),
 ]

e8e06f4c8a70c213d78d505e68fb38ae2f251548
danieldeyisus <danieldejesusgs@gmail.com>
2020-05-05 17:54:47 -0400

    :sparkles: arreglos login y diseño en general

--- registration/forms.py
+++ registration/forms.py
@@ -32,14 +32,14 @@ class UsuarioRegisterForm(forms.ModelForm):
             'rol'
         ]
         widgets = {
-            'rut_usuario': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa RUT '}),
-            'primer_nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa primer nombre'}),
-            'segundo_nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa segundo nombre'}),
-            'primer_apellido': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa primer apellido'}),
-            'segundo_apellido': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa segundo apellido'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa correo'}),
-            'telefono': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa telefono'}),
-            'rol': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Ingresa un rol'}),
+            'rut_usuario': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa RUT', 'pattern': '^[0-9]{8,9}[-|‐]{1}[0-9kK]{1}$'}),
+            'primer_nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa primer nombre', 'pattern': '[A-Za-z ]+'}),
+            'segundo_nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa segundo nombre', 'pattern': '[A-Za-z ]+'}),
+            'primer_apellido': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa primer apellido', 'pattern': '[A-Za-z ]+'}),
+            'segundo_apellido': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa segundo apellido', 'pattern': '[A-Za-z ]+'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa correo', 'pattern': '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'}),
+            'telefono': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa telefono', 'min': '9'}),
+            'rol': forms.Select(attrs={'class': 'form-control'}),
         }
     
     def clean_password2(self):

1e85db07ddb59f7fa2904be0fd4d7184ad97132f
danieldeyisus <danieldejesusgs@gmail.com>
2020-06-18 01:56:14 -0400

    :bug: se arregla error de footer

--- dashboard/static/dashboard/js/datatables/datatable.js
+++ dashboard/static/dashboard/js/datatables/datatable.js
@@ -239,3 +239,63 @@ $(document).ready(function () {
     }
   });
 });
+
+$(document).ready(function () {
+  $('#table_presupuesto').DataTable({
+    "language": {
+      "lengthMenu": "Mostrar _MENU_ registros",
+      "zeroRecords": "No se encontraron resultados",
+      "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+      "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+      "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+      "sSearch": "Buscar:",
+      "oPaginate": {
+        "sFirst": "Primero",
+        "sLast": "Último",
+        "sNext": "Siguiente",
+        "sPrevious": "Anterior"
+      },
+      "sProcessing": "Procesando...",
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('#table_siniestros_inspecc').DataTable({
+    "language": {
+      "lengthMenu": "Mostrar _MENU_ registros",
+      "zeroRecords": "No se encontraron resultados",
+      "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+      "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+      "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+      "sSearch": "Buscar:",
+      "oPaginate": {
+        "sFirst": "Primero",
+        "sLast": "Último",
+        "sNext": "Siguiente",
+        "sPrevious": "Anterior"
+      },
+      "sProcessing": "Procesando...",
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('#table_siniestro_reparado').DataTable({
+    "language": {
+      "lengthMenu": "Mostrar _MENU_ registros",
+      "zeroRecords": "No se encontraron resultados",
+      "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+      "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+      "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+      "sSearch": "Buscar:",
+      "oPaginate": {
+        "sFirst": "Primero",
+        "sLast": "Último",
+        "sNext": "Siguiente",
+        "sPrevious": "Anterior"
+      },
+      "sProcessing": "Procesando...",
+    }
+  });
+});

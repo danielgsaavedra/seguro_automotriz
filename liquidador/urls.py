a2609f75185936549f390177200af373eaaa5fec
Jota-Wolf <lobos.joaquin@hotmail.com>
2020-06-25 01:45:50 -0400

    :sparkles: Se añade modulo para generar reportes de actas,informe daños y presupuestos al liquidador

--- liquidador/urls.py
+++ liquidador/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('siniestros/', views.SiniestroView, name='siniestros_liquidador'),
     path('presupuestos/', views.PresupuestoView, name='presupuestos_liquidador'),
     path('siniestros/disabled', views.SiniestroDisabledView, name='siniestros_disabled_liquidador'),
     path('presupuestos/aprobados', views.PresupuestoAprobadoView, name='presupuestos_aprobados'),
     path('presupuestos/rechazados', views.PresupuestoRechazadoView, name='presupuestos_rechazados'),
+    path('reportes/', views.ReportesView, name='reportes'),
 ]
